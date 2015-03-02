94e4971088175425343c5e00affb26444f16859c
David Hasenjaeger <davehasen@gmail.com>
2015-03-02 01:07:25 -0800

    most tests passed

--- complexNumber.js
+++ complexNumber.js
@@ -1,3 +1,19 @@
+var tol = 15;
+
+function checkIfZero (argument) {
+	var i = argument;
+	var j = -argument;
+	var test = 0;
+	if (i.toFixed(tol) === test.toFixed(tol)) {
+		i = 0;
+	} else if (j.toFixed(tol) === test.toFixed(tol)) {
+		i = 0;
+	} else {
+		console.log(i.toFixed(tol));
+	}
+	return i;
+}
+
 function PolarNumber(a, b) {
 	var th;
 	if (b > 1 || b < -1) {
@@ -9,8 +25,8 @@ function PolarNumber(a, b) {
 	var number = [a, th];
 
 	number.textVersion = function () {
-		var rounded0 = Number(this[0].toFixed(15));
-		var rounded1 = Number(this[1].toFixed(15));
+		var rounded0 = Number(this[0].toFixed(tol));
+		var rounded1 = Number(this[1].toFixed(tol));
 		return rounded0 + 'e^(' + rounded1 + decodeURI(encodeURI('\u03C0')) + 'i)'
 	};
 
@@ -22,6 +38,7 @@ function PolarNumber(a, b) {
 	number.rectangular = function () {
 		var r = this[0] * Math.cos(Math.PI * this[1]);
 		var i = this[0] * Math.sin(Math.PI * this[1]);
+		i = checkIfZero(i);
 		return ComplexNumber(r, i);
 	};
 
@@ -47,14 +64,13 @@ function ComplexNumber(a, b) {
 		var rounded0 = Number(this[0].toFixed(15));
 		var rounded1 = Number(this[1].toFixed(15));
 		var str = '';
+		if (rounded1 === 1) {
+			rounded1 = '';
+		}
 		if (rounded1 === 0) {
 			str += rounded0;
 		} else if (rounded0 === 0) {
-			if (rounded1 === 1) {
-				str += 'i';
-			} else {
-				str += rounded1 + 'i';
-			}
+			str += rounded1 + 'i';
 		} else {
 			str += rounded0 + ' + ' + rounded1 + 'i';
 		}
@@ -69,6 +85,7 @@ function ComplexNumber(a, b) {
 	number.plus = function(arr) {
 		var r = this[0] + arr[0];
 		var i = this[1] + arr[1];
+		i = checkIfZero(i);
 		return ComplexNumber(r, i);
 	};
 
@@ -81,6 +98,7 @@ function ComplexNumber(a, b) {
 	number.times = function (arr) {
 		var r = this[0] * arr[0] - this[1] * arr[1];
 		var i = this[0] * arr[1] + this[1] * arr[0];
+		i = checkIfZero(i);
 		return ComplexNumber(r, i);
 	};
 
